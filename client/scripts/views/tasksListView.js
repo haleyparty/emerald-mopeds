6b769fc4b5c0507e3ddbaaba715f47327e71728b
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-11-30 16:01:26 -0800

    displaying total ammount on job view

--- client/scripts/views/tasksListView.js
+++ client/scripts/views/tasksListView.js
@@ -13,6 +13,7 @@ Lancealot.TasksListView = Backbone.View.extend({
 
   initialize: function(options){
     this.url = options.jobId;
+    this.total = 0;
     this.collection.on('sync', this.render, this);
     this.collection.fetch({
       url: '/api/job/' + this.url
@@ -31,11 +32,20 @@ Lancealot.TasksListView = Backbone.View.extend({
 
   addAll: function(){
     this.collection.forEach(this.addOne, this);
+    // this.$el.html(this.template(this.options));
   },
 
   render: function(){
     this.$el.empty();
-    this.$el.html(this.template());
+    this.collection.forEach(function(item) {
+      console.log(item)
+      this.total += item.attributes.expenses.reduce(function (acc, expense) {
+        return acc + expense.unit_price;
+      }, 0) + item.attributes.employees.reduce(function (acc, employee) {
+        return acc + employee._pivot_time_spent * employee.hourly_billing_fee;
+      }, 0);
+    }.bind(this));
+    this.$el.html(this.template({total: this.total}));
     this.addAll();
     return this;
   },
